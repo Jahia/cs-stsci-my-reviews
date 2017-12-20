e87ab67ee0788c711f30d933369eba7a4d2b6ce4
Axel BRUNNER <abrunner@jahia.com>
2017-12-20 10:09:18 +0100

    Correct processId and not TaskID

--- src/main/java/org/jahia/modules/reviews/actions/CommentWorkflowAction.java
+++ src/main/java/org/jahia/modules/reviews/actions/CommentWorkflowAction.java
@@ -20,12 +20,17 @@ public class CommentWorkflowAction extends Action {
         String          nodeId          =       getParameter(parameters, "node");            // NodeId of the workflow
         String          comment         =       getParameter(parameters,"comment");          // Comment of the workflow
         JCRNodeWrapper  task            =       session.getNodeByIdentifier(nodeId);                    // WorkflowTask to get the process ID, which is the processID
+
         if(task.getPrimaryNodeTypeName().equals("jnt:workflowTask")){
             String          userName        =       session.getUser().getUsername();                    // Username
             String          provider        =       "jBPM";                                             //Provider, need to add a comment
-            String          processID       =       task.getPropertyAsString("taskId");           //ProcessId, need to add a comment
+            String          taskId          =       task.getPropertyAsString("taskId");           //ProcessId, need to add a comment
+            String          processID       =       null;
+            List<WorkflowTask> tasks        =       WorkflowService.getInstance().getTasksForUser(session.getUser(),session.getLocale());
 
-            if (null != comment && !comment.isEmpty()) {
+            processID = getProcessId(tasks,taskId);
+
+            if (null != comment && !comment.isEmpty() && processID!= null) {
                 WorkflowService.getInstance().addComment(processID,provider,comment,userName);
             }else {
                 return ActionResult.INTERNAL_ERROR_JSON;
@@ -35,5 +40,14 @@ public class CommentWorkflowAction extends Action {
             return ActionResult.INTERNAL_ERROR_JSON;
         }
 
+    }
+
+    private String getProcessId(List<WorkflowTask> tasks, String taskId){
+        for (WorkflowTask taskWorkflow : tasks){
+            if(taskWorkflow.getId().equals(taskId)){
+               return taskWorkflow.getProcessId();
+            }
+        }
+        return null;
     }
 }
